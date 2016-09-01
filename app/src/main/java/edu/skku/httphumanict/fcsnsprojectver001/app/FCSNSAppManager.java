6e585b4010379a137554e2532f8fcf60b94baf38
ljs0322 <handicapped2@gmail.com>
2016-09-01 17:38:25 +0900

    :

---
+++ app/src/main/java/edu/skku/httphumanict/fcsnsprojectver001/app/FCSNSAppManager.java
@@ -0,0 +1,8 @@
+package edu.skku.httphumanict.fcsnsprojectver001.app;
+
+/**
+ *
+ * Created by ProLab on 2016-07-26.
+ */
+public class FCSNSAppManager {
+}// end of class
---
+++ app/src/main/java/edu/skku/httphumanict/fcsnsprojectver001/app/activity/FCSNSLogoActivity.java
@@ -0,0 +1,15 @@
+package edu.skku.httphumanict.fcsnsprojectver001.app.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.skku.httphumanict.fcsnsprojectver001.R;
+
+public class FCSNSLogoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fcsns_logo);
+    }
+}// end of class
